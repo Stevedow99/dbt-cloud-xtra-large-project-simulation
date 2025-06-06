e34c24107cebf07ba7a4fcff883d39fc66fee909
Steve Dowling <steve.dowling@dbtlabs.com>
2025-06-06 19:10:38 -0400

    updates

--- models/dashboard_connected_models/dashboard__model_one_thousand,_four_hundred_and_fifty_nine.sql
+++ models/dashboard_connected_models/dashboard__model_one_thousand,_four_hundred_and_fifty_nine.sql
@@ -16,7 +16,7 @@ with model_a as
           c.*
    from model_a a
    inner join model_b b on a.model_a_unqiue_key = b.model_b_unqiue_key
-   inner join model_c c on a.model_a_unqiue_key = b.model_c_unqiue_key)
+   inner join model_c c on a.model_a_unqiue_key = c.model_c_unqiue_key)
 select * exclude (model_a_unqiue_key, model_b_unqiue_key, model_c_unqiue_key),
          row_number() over (partition by 1
                             order by 1) as unqiue_key
--- models/staging/sample_customer_data/stg__sample_customer_data_customer_dim.sql
+++ models/staging/sample_customer_data/stg__sample_customer_data_customer_dim.sql
@@ -23,7 +23,6 @@ renamed as (
         cc_type,
         cc_number,
         cc_cvc,
-        cc_expiredate,
         customer_join_date,
         row_number() over (partition by 1 order by 1) as unqiue_key
 
